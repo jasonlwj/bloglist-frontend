74e98728e4c9f12ce3480f619001fd0779f65161
Jason Liew <jason.97.liew@gmail.com>
2020-12-09 10:55:47 +1100

    5.1: bloglist frontend, step1

--- src/App.js
+++ src/App.js
@@ -1,66 +1,98 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 import './App.css'
 
 const LoginForm = ({ handleLogin, username, password, handleUsernameChange, handlePasswordChange }) => (
-  <div>
-    <h2>log in to application</h2>
-    <form onSubmit={handleLogin}>
-      <div>
-        username <input type="text" value={username} onChange={handleUsernameChange} />
-      </div>
-      <div>
-        password <input type="password" value={password} onChange={handlePasswordChange} />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  </div>
+	<div>
+		<h2>log in to application</h2>
+		<form onSubmit={handleLogin}>
+			<div>
+				username <input type="text" value={username} onChange={handleUsernameChange} />
+			</div>
+			<div>
+				password <input type="password" value={password} onChange={handlePasswordChange} />
+			</div>
+			<button type="submit">login</button>
+		</form>
+	</div>
 )
 
 const BlogList = ({ blogs }) => (
-  <div>
-    <h2>blogs</h2>
-    {blogs.map(blog =>
-      <Blog key={blog.id} blog={blog} />
-    )}
-  </div>
+	<div>
+		<h2>blogs</h2>
+		{blogs.map(blog =>
+			<Blog key={blog.id} blog={blog} />
+		)}
+	</div>
 )
 
 const App = () => {
-  const [ blogs, setBlogs ] = useState([])
-  const [ username, setUsername ] = useState('')
-  const [ password, setPassword ] = useState('')
-  const [ user, setUser ] = useState(null)
-  
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-      )  
-    }, [])
-    
-  const handleUsernameChange = ({ target }) => setUsername(target.value)
+	const [ blogs, setBlogs ] = useState([])
+	const [ username, setUsername ] = useState('')
+	const [ password, setPassword ] = useState('')
+	const [ user, setUser ] = useState(null)
+	
+	// get all blogs
+	useEffect(() => {
+		blogService.getAll().then(blogs =>
+			setBlogs( blogs )
+		)  
+	}, [])
+		
+	// check if user is logged in
+	useEffect(() => {
+		const userLoggedIn = window.localStorage.getItem('userLoggedIn')
 
-  const handlePasswordChange = ({ target }) => setPassword(target.value)
+		if (userLoggedIn) {
+			const userJSON = JSON.parse(userLoggedIn)
+			setUser(userJSON)
+		}
+	}, [])
+	
+	const handleUsernameChange = ({ target }) => setUsername(target.value)
 
-  const handleLogin = () => {
-    console.log('logging in with', username, password)
-  }
+	const handlePasswordChange = ({ target }) => setPassword(target.value)
 
-  return (
-    <div className="App">
-      <div>
-        <LoginForm
-          handleLogin={handleLogin}
-          username={username}
-          password={password}
-          handleUsernameChange={handleUsernameChange}
-          handlePasswordChange={handlePasswordChange}
-        />
-        <BlogList blogs={blogs} />
-      </div>
-    </div>
-  )
+	const handleLogin = async event => {
+		event.preventDefault()
+
+		try {
+			const user = await loginService.login({
+				username, 
+				password
+			})
+
+			window.localStorage.setItem(
+				'userLoggedIn', JSON.stringify(user)
+			)
+
+			setUser(user)
+			setUsername('')
+			setPassword('')
+		} catch (error) {
+			console.log('Invalid credentials')
+		}
+	}
+
+	return (
+		<div className="App">
+			<div>
+				{
+					!user 
+						? <LoginForm
+							handleLogin={handleLogin}
+							username={username}
+							password={password}
+							handleUsernameChange={handleUsernameChange}
+							handlePasswordChange={handlePasswordChange}
+						/>
+						: <BlogList blogs={blogs} />
+				}
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://localhost:3003/api/blogs'
+const baseUrl = 'http://localhost:3003/api/blogs'
 
 const getAll = () => {
   const request = axios.get(baseUrl)

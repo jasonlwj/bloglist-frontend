017dcc7e35f1590016f220c8df307fbde4976e52
Jason Liew <jason.97.liew@gmail.com>
2020-12-08 17:30:28 +1100

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,64 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import './App.css'
+
+const LoginForm = ({ handleLogin, username, password, handleUsernameChange, handlePasswordChange }) => (
+  <div>
+    <h2>log in to application</h2>
+    <form onSubmit={handleLogin}>
+      <div>
+        username <input type="text" value={username} onChange={handleUsernameChange} />
+      </div>
+      <div>
+        password <input type="password" value={password} onChange={handlePasswordChange} />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+)
+
+const BlogList = ({ blogs }) => (
+  <div>
+    <h2>blogs</h2>
+    {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )}
+  </div>
+)
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-
+  const [ blogs, setBlogs ] = useState([])
+  const [ username, setUsername ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ user, setUser ] = useState(null)
+  
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
-    )  
-  }, [])
+      )  
+    }, [])
+    
+  const handleUsernameChange = ({ target }) => setUsername(target.value)
+
+  const handlePasswordChange = ({ target }) => setPassword(target.value)
+
+  const handleLogin = () => {
+    console.log('logging in with', username, password)
+  }
 
   return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+    <div className="App">
+      <div>
+        <LoginForm
+          handleLogin={handleLogin}
+          username={username}
+          password={password}
+          handleUsernameChange={handleUsernameChange}
+          handlePasswordChange={handlePasswordChange}
+        />
+        <BlogList blogs={blogs} />
+      </div>
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-const baseUrl = '/api/blogs'
+
+const baseUrl = 'https://localhost:3003/api/blogs'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
 export default { getAll }
